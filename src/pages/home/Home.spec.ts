a8002f97efd4ee72cb112093bfa87cf4491ea1cf
LeoSouza221 <leosouza221@gmail.com>
2024-04-23 15:22:30 -0300

    test: add new tests to home page

--- src/pages/home/Home.spec.ts
+++ src/pages/home/Home.spec.ts
@@ -1,6 +1,6 @@
 import { render, waitFor, screen } from '@testing-library/vue';
 import { setupServer } from 'msw/node';
-import { HttpResponse, http } from 'msw';
+import { HttpResponse, delay, http } from 'msw';
 import userEvent from '@testing-library/user-event';
 import Home from './Home.vue';
 
@@ -30,18 +30,28 @@ beforeAll(() => server.listen());
 afterAll(() => server.close());
 
 describe.only('Home', () => {
-  it('send request when load page', async () => {
-    render(Home);
+  describe('when load page', () => {
+    it('should send requests', async () => {
+      render(Home);
 
-    await waitFor(() => {
-      expect(counter).toBe(3);
+      await waitFor(() => {
+        expect(counter).toBe(3);
+      });
     });
-  });
 
-  it('should be loading when open the screen', () => {
-    render(Home);
+    it('should be loading page', () => {
+      render(Home);
 
-    expect(screen.queryByRole('status')).toBeInTheDocument();
+      expect(screen.queryByRole('status')).toBeInTheDocument();
+    });
+
+    it('should have input value', async () => {
+      render(Home);
+
+      const element = await screen.findByPlaceholderText('Username');
+
+      expect(element).toHaveValue('LeoSouza221');
+    });
   });
 
   describe('when success to load user', () => {
@@ -121,5 +131,109 @@ describe.only('Home', () => {
         expect(screen.queryAllByText(/starred/).length).toBe(3);
       });
     });
+  });
+  describe('when enter a new user', () => {
+    it('should do new requests', async () => {
+      render(Home);
+
+      const input = await screen.findByPlaceholderText('Username');
+      const button = screen.getByRole('button', { name: 'Search' });
+
+      input.setSelectionRange(0, 10);
+      userEvent.type(input, '{backspace}teste');
+
+      userEvent.click(button);
+
+      server.use(
+        http.get('api/users/teste', async ({}) => {
+          counter += 1;
+          return HttpResponse.json();
+        }),
+        http.get('api/users/teste/repos', async ({}) => {
+          counter += 1;
+          return HttpResponse.json();
+        }),
+        http.get('api/users/teste/starred', async ({}) => {
+          counter += 1;
+          return HttpResponse.json();
+        }),
+      );
+
+      await waitFor(() => {
+        expect(counter).toBe(6);
+      });
+    });
+    it('should show loading when searching', async () => {
+      render(Home);
+
+      const input = await screen.findByPlaceholderText('Username');
+      const button = screen.getByRole('button', { name: 'Search' });
+
+      input.setSelectionRange(0, 10);
+      userEvent.type(input, '{backspace}teste');
+
+      userEvent.click(button);
+
+      server.use(
+        http.get('api/users/teste', async ({}) => {
+          delay('infinite');
+          return HttpResponse.json({ name: 'teste' });
+        }),
+        http.get('api/users/teste/repos', async ({}) => {
+          delay('infinite');
+          return HttpResponse.json();
+        }),
+        http.get('api/users/teste/starred', async ({}) => {
+          delay('infinite');
+          return HttpResponse.json();
+        }),
+      );
+
+      await waitFor(() => {
+        expect(screen.queryByRole('status')).toBeInTheDocument();
+      });
+    });
+    // it('should get new user infos', async () => {
+    //   render(Home);
+
+    //   const input = await screen.findByPlaceholderText('Username');
+    //   const button = screen.getByRole('button', { name: 'Search' });
+
+    //   input.setSelectionRange(0, 10);
+    //   userEvent.type(input, '{backspace}basarbk');
+
+    //   userEvent.click(button);
+
+    //   let resolveFunc;
+    //   const promise = new Promise((resolve) => {
+    //     resolveFunc = resolve;
+    //   });
+
+    //   server.use(
+    //     http.get('api/users/basarbk', async ({}) => {
+    //       await promise;
+    //       return HttpResponse.json({ name: 'Basar' });
+    //     }),
+    //   );
+
+    //   // @ts-ignore
+    //   await resolveFunc();
+
+    //   await waitFor(() => {
+    //     expect(screen.queryByText('Basar')).toBeInTheDocument();
+    //   });
+    // });
+  });
+  it('show error when leave input empty', async () => {
+    render(Home);
+
+    const input = await screen.findByPlaceholderText('Username');
+    const button = screen.getByRole('button', { name: 'Search' });
+
+    input.setSelectionRange(0, 12);
+    userEvent.type(input, '{backspace}');
+    userEvent.click(button);
+
+    expect(screen.queryByText('Enter a username')).toBeInTheDocument();
   });
 });
